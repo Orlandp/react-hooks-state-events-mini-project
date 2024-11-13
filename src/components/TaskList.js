3139f5d58745ee3408073831d036fd435ed9e764
pknowledge <jamesblssings@gmail.com>
2024-11-13 03:26:28 +0300

    creating hooks and state

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,9 +1,21 @@
 import React from "react";
+import Task from "./Task";
 
-function TaskList() {
+function TaskList({ tasks, selectedCategory, onDeleteTask }) {
+  const tasksToDisplay = tasks.filter((task) => 
+    selectedCategory === "All" || task.category === selectedCategory
+  );
+
   return (
     <div className="tasks">
-      {/* display a list of tasks using Task component */}
+      {tasksToDisplay.map((task) => (
+        <Task
+          key={task.text}
+          text={task.text}
+          category={task.category}
+          onDeleteTask={onDeleteTask}
+        />
+      ))}
     </div>
   );
 }
